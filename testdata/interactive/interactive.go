11cb791be146a6fa160e17dae3a62847c6d04830
Justin Ginn <Justindavid.ginn@gmail.com>
2025-03-17 11:04:11 +0100

    Remove unused testdata/interactive

--- testdata/interactive/interactive.go
+++
@@ -1,9 +0,0 @@
-package interactive
-
-import (
-	goroom "github.com/jdginn/go-recording-studio/room"
-)
-
-func interact(arrivals []goroom.Arrival) error {
-	tea.NewProgram(interactModel{arrivals: arrivals}).Start()
-}
