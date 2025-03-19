bfdd120db02a847df3458253159b7f9f5510bd43
Justin Ginn <Justindavid.ginn@gmail.com>
2025-03-19 15:16:40 +0100

    room: add some analysis features

---
+++ room/analysis.go
@@ -0,0 +1,17 @@
+package room
+
+import (
+	"math"
+)
+
+const MS float64 = 1.0 / 1000.0
+
+func TotalGainOverWindow(arrivals []Arrival, windowMS float64, floor float64) (float64, error) {
+	totalGain := 0.0
+	for _, arrival := range arrivals {
+		if arrival.Distance/SPEED_OF_SOUND/MS < windowMS {
+			totalGain = totalGain + (math.Abs(floor - toDB(arrival.Gain)))
+		}
+	}
+	return totalGain, nil
+}
